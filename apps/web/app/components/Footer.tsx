2195ba9ff8167bfe3e358553fd360615e401c936
Milan Kopp <milan.kopp@monetplus.cz>
2025-07-08 00:01:36 +0200

    refactor: odstranění lokace z footeru
    - Odstraněna informace o lokaci Zlín z kontaktní sekce
    - Lokace není relevantní pro online rezervační systém
    - Footer nyní obsahuje pouze email a dostupnost systému
    - Čistší a relevantnější design

--- apps/web/app/components/Footer.tsx
+++ apps/web/app/components/Footer.tsx
@@ -109,15 +109,6 @@ export default function Footer() {
                   </a>
                 </div>
               </div>
-              
-              <div className="flex items-start space-x-2">
-                <svg className="w-4 h-4 mt-0.5 text-orange-400" fill="currentColor" viewBox="0 0 20 20">
-                  <path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd"/>
-                </svg>
-                <div>
-                  <span>Zlín, Česká republika</span>
-                </div>
-              </div>
 
               <div className="flex items-start space-x-2">
                 <svg className="w-4 h-4 mt-0.5 text-orange-400" fill="currentColor" viewBox="0 0 20 20">
